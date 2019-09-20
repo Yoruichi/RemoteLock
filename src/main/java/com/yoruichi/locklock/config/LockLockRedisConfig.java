576f6e245b7c816ddabaff45b12954756dae972d
wangguowen <wangguowen@superatomfin.com>
2019-09-20 12:28:36 +0800

    add pool config

--- src/main/java/com/yoruichi/locklock/config/LockLockRedisConfig.java
+++ src/main/java/com/yoruichi/locklock/config/LockLockRedisConfig.java
@@ -6,9 +6,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisClientConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import redis.clients.jedis.JedisPoolConfig;
 
 import java.util.Objects;
 
@@ -21,8 +23,18 @@ public class LockLockRedisConfig {
             @Value("${spring.redis.host:localhost}") String host,
             @Value("${spring.redis.port:6379}") int port,
             @Value("${spring.redis.database.locklock:0}") int db,
-            @Value("${spring.redis.password:}") String password
+            @Value("${spring.redis.password:}") String password,
+            @Value("${spring.redis.active.locklock:64}") int maxActive,
+            @Value("${spring.redis.wait.locklock:64}") int maxWait,
+            @Value("${spring.redis.idle.max.locklock:64}") int maxIdle,
+            @Value("${spring.redis.idle.min.locklock:32}") int minIdle
     ) {
+
+        JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
+        jedisPoolConfig.setMaxTotal(maxActive);
+        jedisPoolConfig.setMaxWaitMillis(maxWait);
+        jedisPoolConfig.setMaxIdle(maxIdle);
+        jedisPoolConfig.setMinIdle(minIdle);
         RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration();
         configuration.setHostName(host);
         configuration.setPort(port);
@@ -30,8 +42,10 @@ public class LockLockRedisConfig {
         if (Objects.nonNull(password) && password.trim().length() > 0) {
             configuration.setPassword(password);
         }
-
-        RedisTemplate template = new StringRedisTemplate(new JedisConnectionFactory(configuration));
+        JedisConnectionFactory factory = new JedisConnectionFactory(configuration);
+        factory.setPoolConfig(jedisPoolConfig);
+        factory.afterPropertiesSet();
+        RedisTemplate template = new StringRedisTemplate(factory);
         logger.info("Create redis template with url {}", ((JedisConnectionFactory) template.getConnectionFactory()).getHostName());
         return template;
     }
